7378c3d61bb219981ad6c8b6bbcac1b2e97d9a02
romero <61641738+romerojhh@users.noreply.github.com>
2026-03-04 23:03:41 -0800

    step 2 v2

---
+++ functions/api/finance.ts
@@ -0,0 +1,8 @@
+export const onRequestGet: PagesFunction = async () => {
+  return new Response('Not implemented', { status: 501 })
+}
+
+export const onRequestPost: PagesFunction = async () => {
+  return new Response('Not implemented', { status: 501 })
+}
+
